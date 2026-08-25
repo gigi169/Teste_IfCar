3b6f1340b2424715afa1c34fba23eda7e14f330f
gigi169 <20241tbor0020038@estudantes.ifpr.edu.br>
2026-08-25 12:19:54 -0300

    Add files via upload

---
+++ Teste_IfCar/chat.php
@@ -0,0 +1,38 @@
+<?php 
+ $requests = [
+
+        [
+            'nome' => 'Maria Silva',
+            'universidade' => 'Universidade Estadual',
+            'origem' => 'Curitiba',
+            'destino' => 'Ponta Grossa',
+            'data' => '28 Ago',
+            'horario' => '07:30',
+            'passageiros' => 1,
+            'descricao' => 'Preciso de uma carona para a faculdade.'
+        ],
+
+        [
+            'nome' => 'João Santos',
+            'universidade' => 'UTFPR',
+            'origem' => 'Castro',
+            'destino' => 'Curitiba',
+            'data' => '29 Ago',
+            'horario' => '06:50',
+            'passageiros' => 2,
+            'descricao' => 'Procuro carona para chegar à universidade pela manhã.'
+        ],
+
+        [
+            'nome' => 'Ana Oliveira',
+            'universidade' => 'Universidade Estadual',
+            'origem' => 'São José dos Pinhais',
+            'destino' => 'Curitiba',
+            'data' => '30 Ago',
+            'horario' => '08:00',
+            'passageiros' => 1,
+            'descricao' => 'Gostaria de dividir uma carona com outros estudantes.'
+        ]
+
+    ];
+?>    
